83ceaa12fd4dbfcc33232c0348ae8553d5f93229
Hugo <hugo.lima.chaves@gmail.com>
2018-07-11 20:25:38 -0300

    Add files via upload

--- trackerRunner.py
+++ trackerRunner.py
@@ -3,10 +3,10 @@ import util as ut
 from PIL import Image
 
 print("comeco codigo")
-caminhoCodigo = '/home/hugo/Documents/Mestrado/codigoRastreador/bin/Debug/tracker'
-caminhoBD = '/home/hugo/Documents/Mestrado/alov_dataset/alov300'
-subpasta = 'videos'
-pastaAlteracao = 'alteracao'
+caminhoCodigo = '/home/hugo/Documents/Mestrado/codigoRastreador/bin/Debug/tracker' # caminho do tracker
+caminhoBD = '/home/hugo/Documents/Mestrado/alov_dataset/alov300' #raiz do banco de dados, do tipo de dados antes das classes
+subpasta = 'videos' # nome da pasta que contem os frames
+pastaAlteracao = 'alteracao' #obsoleto
 
 def videoGrande(caminhoBD,subpasta,categoria,video,QualOTamanhoDeUmVideoGrandeEmMB):
     items = os.listdir(os.path.join(os.path.join(os.path.join(caminhoBD,subpasta),categoria),video))
@@ -39,18 +39,20 @@ def _main():
         for video in videos:
 
             print('Executando tracker para o video: ', video,', da categoria: ',categoria)
-            if videoGrande(caminhoBD,subpasta,categoria,video,4000): #4000MB de memoria o video ja e grande
+            ''' if videoGrande(caminhoBD,subpasta,categoria,video,4000): #4000MB de memoria o video ja e grande
                 print('Por enquanto vamos pular esse video')
                 continue
+            
             else:
-
-                caminhoVideo = os.path.join(os.path.join(os.path.join(caminhoBD,subpasta),categoria),video)
-                caminhoParam = os.path.join(caminhoVideo,'parameters.yml')
-                if not (os.path.exists(os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao))):
-                    os.makedirs(os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao))
-                    caminhoAlteracao = os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao)
-                comando = caminhoCodigo + ' ' + caminhoParam + ' ' + caminhoAlteracao
-                os.system(comando)
+            '''
+            caminhoVideo = os.path.join(os.path.join(os.path.join(caminhoBD,subpasta),categoria),video)
+            caminhoParam = os.path.join(caminhoVideo,'parameters.yml')
+            caminhoGravacaoObjMdel = os.path.join(caminhoVideo,'objectModelCoordenates.txt')
+            if not (os.path.exists(os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao))):
+                os.makedirs(os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao))
+                caminhoAlteracao = os.path.join(caminhoBD,subpasta,categoria,video,pastaAlteracao)
+            comando = caminhoCodigo + ' ' + caminhoParam + ' ' + caminhoGravacaoObjMdel
+            os.system(comando)
 
 if __name__ == '__main__':
     _main()
